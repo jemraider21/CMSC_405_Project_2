ba6e5094a31b41584193948e02b35f47ccfa2a61
jemraider21 <jemraider21@gmail.com>
2023-11-14 11:57:26 -0500

    Moved the drawn methods into the TwoDShapeDrawer class

--- app/src/main/java/cmsc_405_project_2/App.java
+++ app/src/main/java/cmsc_405_project_2/App.java
@@ -3,19 +3,14 @@ package cmsc_405_project_2;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
+
+import cmsc_405_project_2.utils.TwoDShapeDrawer;
+
 import javax.swing.*;
 
 public class App extends JFrame implements GLEventListener {
 
-    private float triangleYPos = 0.0f;
-    private boolean triangleGoingUp = true;
-    private float squareXPos = -1.0f;
-    private boolean squareGoingRight = true;
-    private static final float TRIANGLE_SPEED = 0.01f;
-    private static final float SQUARE_SPEED = 0.01f;
-    private float diagonalMovement = -1.0f; // Start from bottom left
-    private float hexagonDiagonalX = 1.0f; // Start from bottom right
-    private float hexagonDiagonalY = -1.0f;
+    transient TwoDShapeDrawer shapeDrawer = new TwoDShapeDrawer();
 
     public App() {
         // Setup OpenGL Version 2
@@ -50,115 +45,10 @@ public class App extends JFrame implements GLEventListener {
         // Clear the drawing area
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 
-        drawMovingTriangle(gl);
-        drawMovingSquare(gl);
-        drawMovingPentagon(gl);
-        drawMovingHexagon(gl);
-    }
-
-    private void drawMovingTriangle(final GL2 gl) {
-        // Reset the current matrix to the "identity"
-        gl.glLoadIdentity();
-
-        // Drawing the red triangle
-        gl.glTranslatef(0.0f, triangleYPos, 0.0f);
-        gl.glColor3f(1.0f, 0.0f, 0.0f); // Set the color to red
-        gl.glBegin(GL.GL_TRIANGLES);
-        gl.glVertex3f(0.0f, 0.5f, 0.0f);
-        gl.glVertex3f(-0.5f, -0.5f, 0.0f);
-        gl.glVertex3f(0.5f, -0.5f, 0.0f);
-        gl.glEnd();
-
-        // Change the Y position of the triangle for the next frame
-        if (triangleGoingUp) {
-            triangleYPos += TRIANGLE_SPEED;
-            if (triangleYPos > 1.0f)
-                triangleGoingUp = false;
-        } else {
-            triangleYPos -= TRIANGLE_SPEED;
-            if (triangleYPos < -1.0f)
-                triangleGoingUp = true;
-        }
-    }
-
-    private void drawMovingSquare(final GL2 gl) {
-        // Reset the current matrix
-        gl.glLoadIdentity();
-
-        // Drawing the blue square
-        gl.glTranslatef(squareXPos, 0.0f, 0.0f);
-        gl.glColor3f(0.0f, 0.0f, 1.0f); // Set the color to blue
-        gl.glBegin(GL2ES3.GL_QUADS);
-        gl.glVertex3f(-0.25f, 0.25f, 0.0f);
-        gl.glVertex3f(-0.25f, -0.25f, 0.0f);
-        gl.glVertex3f(0.25f, -0.25f, 0.0f);
-        gl.glVertex3f(0.25f, 0.25f, 0.0f);
-        gl.glEnd();
-
-        // Change the X position of the square for the next frame
-        if (squareGoingRight) {
-            squareXPos += SQUARE_SPEED;
-            if (squareXPos > 1.0f)
-                squareGoingRight = false;
-        } else {
-            squareXPos -= SQUARE_SPEED;
-            if (squareXPos < -1.0f)
-                squareGoingRight = true;
-        }
-    }
-
-    private void drawMovingPentagon(final GL2 gl) {
-        // Reset the current matrix
-        gl.glLoadIdentity();
-
-        // Drawing the yellow pentagon
-        float pentagonSize = 0.2f; // Size of the pentagon
-        float pentagonXPos = diagonalMovement; // X position for the pentagon
-        float pentagonYPos = diagonalMovement; // Y position for the pentagon
-
-        gl.glTranslatef(pentagonXPos, pentagonYPos, 0.0f);
-        gl.glColor3f(1.0f, 1.0f, 0.0f); // Set the color to yellow
-        gl.glBegin(GL2.GL_POLYGON);
-        for (int i = 0; i < 5; i++) {
-            gl.glVertex2d(
-                    pentagonSize * Math.cos(i * 2 * Math.PI / 5),
-                    pentagonSize * Math.sin(i * 2 * Math.PI / 5));
-        }
-        gl.glEnd();
-
-        // Pentagon's position logic...
-        if (diagonalMovement < 1.0f) {
-            diagonalMovement += 0.005f; // Increase the diagonal position
-        } else {
-            diagonalMovement = -1.0f; // Reset to start again from bottom left
-        }
-    }
-
-    private void drawMovingHexagon(final GL2 gl) {
-        // Reset the current matrix
-        gl.glLoadIdentity();
-
-        // Drawing the green hexagon
-        float hexagonSize = 0.2f; // Size of the hexagon
-
-        gl.glTranslatef(hexagonDiagonalX, hexagonDiagonalY, 0.0f);
-        gl.glColor3f(0.0f, 1.0f, 0.0f); // Set the color to green
-        gl.glBegin(GL2.GL_POLYGON);
-        for (int i = 0; i < 6; i++) {
-            gl.glVertex2d(
-                    hexagonSize * Math.cos(i * 2 * Math.PI / 6),
-                    hexagonSize * Math.sin(i * 2 * Math.PI / 6));
-        }
-        gl.glEnd();
-
-        // Hexagon's position logic...
-        if (hexagonDiagonalX > -1.0f && hexagonDiagonalY < 1.0f) {
-            hexagonDiagonalX -= 0.005f; // Decrease the X position
-            hexagonDiagonalY += 0.005f; // Increase the Y position
-        } else {
-            hexagonDiagonalX = 1.0f; // Reset to start again from bottom right
-            hexagonDiagonalY = -1.0f;
-        }
+        shapeDrawer.drawMovingTriangle(gl);
+        shapeDrawer.drawMovingSquare(gl);
+        shapeDrawer.drawMovingPentagon(gl);
+        shapeDrawer.drawMovingHexagon(gl);
     }
 
     @Override
--- app/src/main/java/cmsc_405_project_2/utils/TwoDShapeDrawer.java
+++ app/src/main/java/cmsc_405_project_2/utils/TwoDShapeDrawer.java
@@ -1,44 +1,131 @@
 package cmsc_405_project_2.utils;
 
+import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GL2ES3;
 
 public class TwoDShapeDrawer {
-    private TwoDShapeDrawer() {
+    // Class member variables for shape positions and directions
+    float triangleYPos = 0.0f;
+    boolean triangleGoingUp = true;
+    float squareXPos = -1.0f;
+    boolean squareGoingRight = true;
+    float pentagonDiagonal = -1.0f;
+    float hexagonDiagonalX = 1.0f;
+    float hexagonDiagonalY = -1.0f;
+
+    private float diagonalMovement = -1.0f; // Start from bottom left
+
+    private static final float TRIANGLE_SPEED = 0.01f;
+    private static final float SQUARE_SPEED = 0.01f;
+
+    public TwoDShapeDrawer() {
+        // Empty constructor
     }
 
-    // Create a method that draws a triangle
-    // You can add this to the display method
-    public static void triangle(GL2 gl2, double r, double g, double b) {
-        gl2.glColor3d(r, g, b);
-        gl2.glBegin(GL2.GL_TRIANGLES);
-        gl2.glVertex3d(-0.5, -0.5, 0);
-        gl2.glVertex3d(0.5, -0.5, 0);
-        gl2.glVertex3d(0, 0.5, 0);
-        gl2.glEnd();
+    public void drawMovingTriangle(final GL2 gl) {
+        // Reset the current matrix to the "identity"
+        gl.glLoadIdentity();
+
+        // Drawing the red triangle
+        gl.glTranslatef(0.0f, triangleYPos, 0.0f);
+        gl.glColor3f(1.0f, 0.0f, 0.0f); // Set the color to red
+        gl.glBegin(GL.GL_TRIANGLES);
+        gl.glVertex3f(0.0f, 0.5f, 0.0f);
+        gl.glVertex3f(-0.5f, -0.5f, 0.0f);
+        gl.glVertex3f(0.5f, -0.5f, 0.0f);
+        gl.glEnd();
+
+        // Change the Y position of the triangle for the next frame
+        if (triangleGoingUp) {
+            triangleYPos += TRIANGLE_SPEED;
+            if (triangleYPos > 1.0f)
+                triangleGoingUp = false;
+        } else {
+            triangleYPos -= TRIANGLE_SPEED;
+            if (triangleYPos < -1.0f)
+                triangleGoingUp = true;
+        }
     }
 
-    // Create a method that draws a square
-    // You can add this to the display method
-    public static void square(GL2 gl2, double r, double g, double b) {
-        gl2.glColor3d(r, g, b);
-        gl2.glBegin(GL2.GL_TRIANGLE_FAN);
-        gl2.glVertex3d(-0.5, -0.5, 0.5);
-        gl2.glVertex3d(0.5, -0.5, 0.5);
-        gl2.glVertex3d(0.5, 0.5, 0.5);
-        gl2.glVertex3d(-0.5, 0.5, 0.5);
-        gl2.glEnd();
+    public void drawMovingSquare(final GL2 gl) {
+        // Reset the current matrix
+        gl.glLoadIdentity();
+
+        // Drawing the blue square
+        gl.glTranslatef(squareXPos, 0.0f, 0.0f);
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Set the color to blue
+        gl.glBegin(GL2ES3.GL_QUADS);
+        gl.glVertex3f(-0.25f, 0.25f, 0.0f);
+        gl.glVertex3f(-0.25f, -0.25f, 0.0f);
+        gl.glVertex3f(0.25f, -0.25f, 0.0f);
+        gl.glVertex3f(0.25f, 0.25f, 0.0f);
+        gl.glEnd();
+
+        // Change the X position of the square for the next frame
+        if (squareGoingRight) {
+            squareXPos += SQUARE_SPEED;
+            if (squareXPos > 1.0f)
+                squareGoingRight = false;
+        } else {
+            squareXPos -= SQUARE_SPEED;
+            if (squareXPos < -1.0f)
+                squareGoingRight = true;
+        }
     }
 
-    // Create a method that draws a pentagon
-    // You can add this to the display method
-    public static void pentagon(GL2 gl2, double r, double g, double b) {
-        gl2.glColor3d(r, g, b);
-        gl2.glBegin(GL2.GL_TRIANGLE_FAN);
-        gl2.glVertex3d(0, 0.5, 0.5);
-        gl2.glVertex3d(-0.5, 0.25, 0.5);
-        gl2.glVertex3d(-0.25, -0.25, 0.5);
-        gl2.glVertex3d(0.25, -0.25, 0.5);
-        gl2.glVertex3d(0.5, 0.25, 0.5);
-        gl2.glEnd();
+    public void drawMovingPentagon(final GL2 gl) {
+        // Reset the current matrix
+        gl.glLoadIdentity();
+
+        // Drawing the yellow pentagon
+        float pentagonSize = 0.2f; // Size of the pentagon
+        float pentagonXPos = diagonalMovement; // X position for the pentagon
+        float pentagonYPos = diagonalMovement; // Y position for the pentagon
+
+        gl.glTranslatef(pentagonXPos, pentagonYPos, 0.0f);
+        gl.glColor3f(1.0f, 1.0f, 0.0f); // Set the color to yellow
+        gl.glBegin(GL2.GL_POLYGON);
+        for (int i = 0; i < 5; i++) {
+            gl.glVertex2d(
+                    pentagonSize * Math.cos(i * 2 * Math.PI / 5),
+                    pentagonSize * Math.sin(i * 2 * Math.PI / 5));
+        }
+        gl.glEnd();
+
+        // Pentagon's position logic...
+        if (diagonalMovement < 1.0f) {
+            diagonalMovement += 0.005f; // Increase the diagonal position
+        } else {
+            diagonalMovement = -1.0f; // Reset to start again from bottom left
+        }
     }
+
+    public void drawMovingHexagon(final GL2 gl) {
+        // Reset the current matrix
+        gl.glLoadIdentity();
+
+        // Drawing the green hexagon
+        float hexagonSize = 0.2f; // Size of the hexagon
+
+        gl.glTranslatef(hexagonDiagonalX, hexagonDiagonalY, 0.0f);
+        gl.glColor3f(0.0f, 1.0f, 0.0f); // Set the color to green
+        gl.glBegin(GL2.GL_POLYGON);
+        for (int i = 0; i < 6; i++) {
+            gl.glVertex2d(
+                    hexagonSize * Math.cos(i * 2 * Math.PI / 6),
+                    hexagonSize * Math.sin(i * 2 * Math.PI / 6));
+        }
+        gl.glEnd();
+
+        // Hexagon's position logic...
+        if (hexagonDiagonalX > -1.0f && hexagonDiagonalY < 1.0f) {
+            hexagonDiagonalX -= 0.005f; // Decrease the X position
+            hexagonDiagonalY += 0.005f; // Increase the Y position
+        } else {
+            hexagonDiagonalX = 1.0f; // Reset to start again from bottom right
+            hexagonDiagonalY = -1.0f;
+        }
+    }
+
 }
