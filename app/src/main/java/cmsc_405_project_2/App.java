4e24f9e94d8471d8dd9a4b9608717135f684520e
jemraider21 <jemraider21@gmail.com>
2023-11-14 11:03:54 -0500

    Created a triangle that moves up and down; created a square that moves left to right

--- app/src/main/java/cmsc_405_project_2/App.java
+++ app/src/main/java/cmsc_405_project_2/App.java
@@ -3,21 +3,25 @@ package cmsc_405_project_2;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
-
 import javax.swing.*;
 
 public class App extends JFrame implements GLEventListener {
 
-    private float yPos = 0.0f;
-    private boolean goingUp = true;
-    private final float speed = 0.01f;
+    private float triangleYPos = 0.0f;
+    private boolean triangleGoingUp = true;
+    private float squareXPos = -1.0f;
+    private boolean squareGoingRight = true;
+    private final float triangleSpeed = 0.01f;
+    private final float squareSpeed = 0.01f;
 
     public App() {
+        // Setup OpenGL Version 2
         GLProfile profile = GLProfile.get(GLProfile.GL2);
         GLCapabilities capabilities = new GLCapabilities(profile);
+        // The canvas is the widget that's drawn in the JFrame
         GLCanvas glCanvas = new GLCanvas(capabilities);
         glCanvas.addGLEventListener(this);
-        this.setName("3D Moving Triangle");
+        this.setName("3D Moving Triangle and Square");
         this.getContentPane().add(glCanvas);
         this.setSize(400, 400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,25 +49,46 @@ public class App extends JFrame implements GLEventListener {
         // Reset the current matrix to the "identity"
         gl.glLoadIdentity();
 
-        // Move the "drawing cursor" around
-        gl.glTranslatef(0.0f, yPos, 0.0f);
-
-        // Draw the triangle
+        // Drawing the triangle
+        gl.glTranslatef(0.0f, triangleYPos, 0.0f);
         gl.glBegin(GL.GL_TRIANGLES);
         gl.glVertex3f(0.0f, 0.5f, 0.0f);
         gl.glVertex3f(-0.5f, -0.5f, 0.0f);
         gl.glVertex3f(0.5f, -0.5f, 0.0f);
         gl.glEnd();
 
-        // Change the Y position for the next frame
-        if (goingUp) {
-            yPos += speed;
-            if (yPos > 1.0f)
-                goingUp = false;
+        // Change the Y position of the triangle for the next frame
+        if (triangleGoingUp) {
+            triangleYPos += triangleSpeed;
+            if (triangleYPos > 1.0f)
+                triangleGoingUp = false;
         } else {
-            yPos -= speed;
-            if (yPos < -1.0f)
-                goingUp = true;
+            triangleYPos -= triangleSpeed;
+            if (triangleYPos < -1.0f)
+                triangleGoingUp = true;
+        }
+
+        // Reset the current matrix
+        gl.glLoadIdentity();
+
+        // Drawing the square
+        gl.glTranslatef(squareXPos, 0.0f, 0.0f);
+        gl.glBegin(GL2ES3.GL_QUADS);
+        gl.glVertex3f(-0.25f, 0.25f, 0.0f);
+        gl.glVertex3f(-0.25f, -0.25f, 0.0f);
+        gl.glVertex3f(0.25f, -0.25f, 0.0f);
+        gl.glVertex3f(0.25f, 0.25f, 0.0f);
+        gl.glEnd();
+
+        // Change the X position of the square for the next frame
+        if (squareGoingRight) {
+            squareXPos += squareSpeed;
+            if (squareXPos > 1.0f)
+                squareGoingRight = false;
+        } else {
+            squareXPos -= squareSpeed;
+            if (squareXPos < -1.0f)
+                squareGoingRight = true;
         }
     }
 
