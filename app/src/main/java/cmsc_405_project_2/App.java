9c7058c18f089c13a511c460e12d77e926f73272
jemraider21 <jemraider21@gmail.com>
2023-11-14 10:50:42 -0500

    Committing to a 2D animation cause time sucks

--- app/src/main/java/cmsc_405_project_2/App.java
+++ app/src/main/java/cmsc_405_project_2/App.java
@@ -1,156 +1,83 @@
 package cmsc_405_project_2;
 
-import java.awt.*;
-import java.awt.event.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
+
 import javax.swing.*;
 
-import cmsc_405_project_2.utils.ThreeDShapeDrawer;
-import com.jogamp.opengl.*;
-import com.jogamp.opengl.awt.*;
+public class App extends JFrame implements GLEventListener {
 
-/**
- * Use JOGL to draw a simple cube
- * with each face being a different color. Rotations
- * can be applied with the arrow keys, the page up
- * key, and the page down key. The home key will set
- * all rotations to 0. Initial rotations about the
- * x, y, and z axes are 15, -15, and 0.
- *
- * This program is meant as an example of using modeling
- * transforms, with glPushMatrix and glPopMatrix.
- *
- * Note that this program does not use lighting.
- */
-public class App extends GLJPanel implements GLEventListener, KeyListener {
+    private float yPos = 0.0f;
+    private boolean goingUp = true;
+    private final float speed = 0.01f;
 
-    /**
-     * A main routine to create and show a window that contains a
-     * panel of type App. The program ends when the
-     * user closes the window.
-     */
-    public static void main(String[] args) {
-        JFrame window = new JFrame("A Simple Unlit Cube -- ARROW KEYS ROTATE");
-        App panel = new App();
-        window.setContentPane(panel);
-        window.pack();
-        window.setLocation(50, 50);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
-        panel.requestFocusInWindow();
-    }
-
-    /**
-     * Constructor for class App.
-     */
     public App() {
-        super(new GLCapabilities(null)); // Makes a panel with default OpenGL "capabilities".
-        setPreferredSize(new Dimension(500, 500));
-        addGLEventListener(this); // A listener is essential! The listener is where the OpenGL programming lives.
-        addKeyListener(this);
+        GLProfile profile = GLProfile.get(GLProfile.GL2);
+        GLCapabilities capabilities = new GLCapabilities(profile);
+        GLCanvas glCanvas = new GLCanvas(capabilities);
+        glCanvas.addGLEventListener(this);
+        this.setName("3D Moving Triangle");
+        this.getContentPane().add(glCanvas);
+        this.setSize(400, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        FPSAnimator animator = new FPSAnimator(glCanvas, 300, true);
+        animator.start();
     }
 
-    // -------------------- methods to draw the cube ----------------------
-
-    double rotateX = 15; // rotations of the cube about the axes
-    double rotateY = -15;
-    double rotateZ = 0;
-
-    
-    
-
-    // -------------------- GLEventListener Methods -------------------------
-
-    /**
-     * The display method is called when the panel needs to be redrawn.
-     * The is where the code goes for drawing the image, using OpenGL commands.
-     */
-    public void display(GLAutoDrawable drawable) {
-
-        GL2 gl2 = drawable.getGL().getGL2(); // The object that contains all the OpenGL methods.
-
-        gl2.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
-
-        gl2.glLoadIdentity(); // Set up modelview transform.
-        gl2.glRotated(rotateZ, 0, 0, 1);
-        gl2.glRotated(rotateY, 0, 1, 0);
-        gl2.glRotated(rotateX, 1, 0, 0);
-
-        ThreeDShapeDrawer.cube(gl2, 1);
-
-        ThreeDShapeDrawer.pyramid(gl2, 1, 0, 0);
-
-        // Add an Index Face set
-        // Note using Graph paper is the best way to figure these vertices.
-        // You can make about any shape you want this way
-        double[][] vertexList = { { 1, -0.5, 1 }, { 1, -.5, -1 }, { 1, .5, -1 }, { 1, .5, 1 }, { .75, .75, 0 },
-                { -.75, .75, 0 }, { -1, -.5, 1 }, { -1, .5, 1 }, { -1, .5, -1 }, { -1, -.5, -1 } };
-
-        int[][] faceList = { { 0, 1, 2, 3 }, { 3, 2, 4 }, { 7, 3, 4, 5 }, { 2, 8, 5, 4 }, { 5, 8, 7 },
-                { 0, 3, 7, 6 }, { 0, 6, 9, 1 }, { 2, 1, 9, 8 }, { 6, 7, 8, 9 } };
-
-        for (int[] ints : faceList) {
-            gl2.glColor3f(1, 0, 1); // Set color for face number i.
-            gl2.glBegin(GL2.GL_TRIANGLE_FAN);
-            for (int j = 0; j < ints.length; j++) {
-                int vertexNum = ints[j]; // Index for vertex j of face i.
-                double[] vertexCoords = vertexList[vertexNum]; // The vertex itself.
-                gl2.glVertex3dv(vertexCoords, 0);
-            }
-            gl2.glEnd();
-
-            // You can add more shapes here.
-
-        }
-
-    } // end display()
-
+    @Override
     public void init(GLAutoDrawable drawable) {
-        // called when the panel is created
-        GL2 gl2 = drawable.getGL().getGL2();
-        gl2.glMatrixMode(GL2.GL_PROJECTION);
-        // gl2.glOrtho(-1, 1 ,-1, 1, -1, 1);
-        // Changing this is your coordinate -x,x,-y,y,-z,z)
-        // Larger numbers zooms out.
-        // Play with this to make sure you see your shapes.
-        gl2.glOrtho(-5, 5, -5, 5, -5, 5);
-        gl2.glMatrixMode(GL2.GL_MODELVIEW);
-        gl2.glClearColor(0, 0, 0, 1);
-        gl2.glEnable(GL2.GL_DEPTH_TEST);
+        // Initialize your OpenGL scene here
     }
 
+    @Override
     public void dispose(GLAutoDrawable drawable) {
-        // called when the panel is being disposed
+        // Cleanup resources here
     }
 
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        final GL2 gl = drawable.getGL().getGL2();
+
+        // Clear the drawing area
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        // Reset the current matrix to the "identity"
+        gl.glLoadIdentity();
+
+        // Move the "drawing cursor" around
+        gl.glTranslatef(0.0f, yPos, 0.0f);
+
+        // Draw the triangle
+        gl.glBegin(GL.GL_TRIANGLES);
+        gl.glVertex3f(0.0f, 0.5f, 0.0f);
+        gl.glVertex3f(-0.5f, -0.5f, 0.0f);
+        gl.glVertex3f(0.5f, -0.5f, 0.0f);
+        gl.glEnd();
+
+        // Change the Y position for the next frame
+        if (goingUp) {
+            yPos += speed;
+            if (yPos > 1.0f)
+                goingUp = false;
+        } else {
+            yPos -= speed;
+            if (yPos < -1.0f)
+                goingUp = true;
+        }
+    }
+
+    @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        // called when user resizes the window
+        // Adjust the viewport or projection matrix here
     }
 
-    // ---------------- Methods from the KeyListener interface --------------
-
-    public void keyPressed(KeyEvent evt) {
-        int key = evt.getKeyCode();
-        if (key == KeyEvent.VK_LEFT)
-            rotateY -= 15;
-        else if (key == KeyEvent.VK_RIGHT)
-            rotateY += 15;
-        else if (key == KeyEvent.VK_DOWN)
-            rotateX += 15;
-        else if (key == KeyEvent.VK_UP)
-            rotateX -= 15;
-        else if (key == KeyEvent.VK_PAGE_UP)
-            rotateZ += 15;
-        else if (key == KeyEvent.VK_PAGE_DOWN)
-            rotateZ -= 15;
-        else if (key == KeyEvent.VK_HOME)
-            rotateX = rotateY = rotateZ = 0;
-        repaint();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new App();
+            }
+        });
     }
-
-    public void keyReleased(KeyEvent evt) {
-    }
-
-    public void keyTyped(KeyEvent evt) {
-    }
-
 }
