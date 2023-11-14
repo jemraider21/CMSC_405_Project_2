6f9ab7556a60cea353f8b43df3501da211d73901
jemraider21 <jemraider21@gmail.com>
2023-11-14 12:09:43 -0500

    Moved the shape properties into their own Properties util class

---
+++ app/src/main/java/cmsc_405_project_2/utils/Properties.java
@@ -0,0 +1,20 @@
+package cmsc_405_project_2.utils;
+
+public class Properties {
+    // Triangle related properties
+    public static final float TRIANGLE_SPEED = 0.01f;
+    public static float triangleYPos = 0.0f;
+    public static boolean triangleGoingUp = true;
+
+    // Square related properties
+    public static final float SQUARE_SPEED = 0.01f;
+    public static float squareXPos = -1.0f;
+    public static boolean squareGoingRight = true;
+
+    // Pentagon related properties
+    public static float pentagonDiagonalMovement = -1.0f; // Start from bottom left
+
+    // Hexagon related properties
+    public static float hexagonDiagonalX = 1.0f;
+    public static float hexagonDiagonalY = -1.0f;
+}
--- app/src/main/java/cmsc_405_project_2/utils/TwoDShapeDrawer.java
+++ app/src/main/java/cmsc_405_project_2/utils/TwoDShapeDrawer.java
@@ -6,18 +6,6 @@ import com.jogamp.opengl.GL2ES3;
 
 public class TwoDShapeDrawer {
     // Class member variables for shape positions and directions
-    float triangleYPos = 0.0f;
-    boolean triangleGoingUp = true;
-    float squareXPos = -1.0f;
-    boolean squareGoingRight = true;
-    float pentagonDiagonal = -1.0f;
-    float hexagonDiagonalX = 1.0f;
-    float hexagonDiagonalY = -1.0f;
-
-    private float diagonalMovement = -1.0f; // Start from bottom left
-
-    private static final float TRIANGLE_SPEED = 0.01f;
-    private static final float SQUARE_SPEED = 0.01f;
 
     public TwoDShapeDrawer() {
         // Empty constructor
@@ -28,7 +16,7 @@ public class TwoDShapeDrawer {
         gl.glLoadIdentity();
 
         // Drawing the red triangle
-        gl.glTranslatef(0.0f, triangleYPos, 0.0f);
+        gl.glTranslatef(0.0f, Properties.triangleYPos, 0.0f);
         gl.glColor3f(1.0f, 0.0f, 0.0f); // Set the color to red
         gl.glBegin(GL.GL_TRIANGLES);
         gl.glVertex3f(0.0f, 0.5f, 0.0f);
@@ -37,14 +25,14 @@ public class TwoDShapeDrawer {
         gl.glEnd();
 
         // Change the Y position of the triangle for the next frame
-        if (triangleGoingUp) {
-            triangleYPos += TRIANGLE_SPEED;
-            if (triangleYPos > 1.0f)
-                triangleGoingUp = false;
+        if (Properties.triangleGoingUp) {
+            Properties.triangleYPos += Properties.TRIANGLE_SPEED;
+            if (Properties.triangleYPos > 1.0f)
+                Properties.triangleGoingUp = false;
         } else {
-            triangleYPos -= TRIANGLE_SPEED;
-            if (triangleYPos < -1.0f)
-                triangleGoingUp = true;
+            Properties.triangleYPos -= Properties.TRIANGLE_SPEED;
+            if (Properties.triangleYPos < -1.0f)
+                Properties.triangleGoingUp = true;
         }
     }
 
@@ -53,7 +41,7 @@ public class TwoDShapeDrawer {
         gl.glLoadIdentity();
 
         // Drawing the blue square
-        gl.glTranslatef(squareXPos, 0.0f, 0.0f);
+        gl.glTranslatef(Properties.squareXPos, 0.0f, 0.0f);
         gl.glColor3f(0.0f, 0.0f, 1.0f); // Set the color to blue
         gl.glBegin(GL2ES3.GL_QUADS);
         gl.glVertex3f(-0.25f, 0.25f, 0.0f);
@@ -63,14 +51,14 @@ public class TwoDShapeDrawer {
         gl.glEnd();
 
         // Change the X position of the square for the next frame
-        if (squareGoingRight) {
-            squareXPos += SQUARE_SPEED;
-            if (squareXPos > 1.0f)
-                squareGoingRight = false;
+        if (Properties.squareGoingRight) {
+            Properties.squareXPos += Properties.SQUARE_SPEED;
+            if (Properties.squareXPos > 1.0f)
+                Properties.squareGoingRight = false;
         } else {
-            squareXPos -= SQUARE_SPEED;
-            if (squareXPos < -1.0f)
-                squareGoingRight = true;
+            Properties.squareXPos -= Properties.SQUARE_SPEED;
+            if (Properties.squareXPos < -1.0f)
+                Properties.squareGoingRight = true;
         }
     }
 
@@ -80,8 +68,8 @@ public class TwoDShapeDrawer {
 
         // Drawing the yellow pentagon
         float pentagonSize = 0.2f; // Size of the pentagon
-        float pentagonXPos = diagonalMovement; // X position for the pentagon
-        float pentagonYPos = diagonalMovement; // Y position for the pentagon
+        float pentagonXPos = Properties.pentagonDiagonalMovement; // X position for the pentagon
+        float pentagonYPos = Properties.pentagonDiagonalMovement; // Y position for the pentagon
 
         gl.glTranslatef(pentagonXPos, pentagonYPos, 0.0f);
         gl.glColor3f(1.0f, 1.0f, 0.0f); // Set the color to yellow
@@ -94,10 +82,10 @@ public class TwoDShapeDrawer {
         gl.glEnd();
 
         // Pentagon's position logic...
-        if (diagonalMovement < 1.0f) {
-            diagonalMovement += 0.005f; // Increase the diagonal position
+        if (Properties.pentagonDiagonalMovement < 1.0f) {
+            Properties.pentagonDiagonalMovement += 0.005f; // Increase the diagonal position
         } else {
-            diagonalMovement = -1.0f; // Reset to start again from bottom left
+            Properties.pentagonDiagonalMovement = -1.0f; // Reset to start again from bottom left
         }
     }
 
@@ -108,7 +96,7 @@ public class TwoDShapeDrawer {
         // Drawing the green hexagon
         float hexagonSize = 0.2f; // Size of the hexagon
 
-        gl.glTranslatef(hexagonDiagonalX, hexagonDiagonalY, 0.0f);
+        gl.glTranslatef(Properties.hexagonDiagonalX, Properties.hexagonDiagonalY, 0.0f);
         gl.glColor3f(0.0f, 1.0f, 0.0f); // Set the color to green
         gl.glBegin(GL2.GL_POLYGON);
         for (int i = 0; i < 6; i++) {
@@ -119,12 +107,12 @@ public class TwoDShapeDrawer {
         gl.glEnd();
 
         // Hexagon's position logic...
-        if (hexagonDiagonalX > -1.0f && hexagonDiagonalY < 1.0f) {
-            hexagonDiagonalX -= 0.005f; // Decrease the X position
-            hexagonDiagonalY += 0.005f; // Increase the Y position
+        if (Properties.hexagonDiagonalX > -1.0f && Properties.hexagonDiagonalY < 1.0f) {
+            Properties.hexagonDiagonalX -= 0.005f; // Decrease the X position
+            Properties.hexagonDiagonalY += 0.005f; // Increase the Y position
         } else {
-            hexagonDiagonalX = 1.0f; // Reset to start again from bottom right
-            hexagonDiagonalY = -1.0f;
+            Properties.hexagonDiagonalX = 1.0f; // Reset to start again from bottom right
+            Properties.hexagonDiagonalY = -1.0f;
         }
     }
 
